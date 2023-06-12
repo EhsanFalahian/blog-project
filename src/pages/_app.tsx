aba2ceeb2610013cb97a99db347a8d612c959d2c
Ehsan <ahmadfallahian.flutter@gmail.com>
2023-06-13 01:13:43 +0430

    add code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Toaster } from 'react-hot-toast';
+import {wrapper} from '../redux/store'
+import AuthProvider from "@/context/AuthProvider";
+import {useEffect} from "react";
+import {loadUser} from "@/redux/user/userActions";
+import {useStore} from "react-redux";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+
+  const store = useStore()
+
+  useEffect(() => {
+    loadUser(store)
+  } , [])
+  return <>
+    <Component {...pageProps} />
+    <Toaster/>
+  </>
+
+
 }
+
+export default wrapper.withRedux(App)
